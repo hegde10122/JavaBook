c2d431ff5eee24f885ef4fac44663c39646ce1c8
hegde10122 <hegde10122@gmail.com>
2022-09-10 17:12:48 +0530

    update
    update

--- CodeSamples/src/Learning.java
+++ CodeSamples/src/Learning.java
@@ -1,7 +1,14 @@
-public class Learning {
+// The first basic Java application.
 
-    public static void main(String[] args) {
+public class Learning
+{
 
-        System.out.println("I am wealthy and rich!!");
+    public static void main(String[] args)
+    {
+        //This prints a statement as the output to the console.
+        System.out.println("I am wealthy and rich");
     }
 }
+
+//OUTPUT OF THE PROGRAM
+// I am wealthy and rich
